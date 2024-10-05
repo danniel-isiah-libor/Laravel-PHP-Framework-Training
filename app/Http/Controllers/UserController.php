b4923172bc3a934566be6f0b776d688e45c6204a
Paul Maitland <paul.ragen.maitland@gmail.com>
2024-10-05 08:56:32 +0800

    day 1

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function submit(Request $request)
+    {
+        // dd($request->age);
+        // dd($request->query('name', 'Danniel'));
+        // dd($request->method());
+        // dd($request->query());
+        // dd($request->all());
+        // dd($request->boolean('name'));
+
+        // $params = ['name', 'age', 'gender'];
+        // dd($request->only($params));
+        // dd($request->only('name', 'age', 'gender'));
+        // dd($request->except($params));
+        // dd($request->has('name'));
+        // dd($request->filled('name'));
+        // dd($request->missing('name'));
+
+        $request->merge(['id' => 1]);
+        dd($request->all());
+    }
+
+    /**
+     * Show the user profile.
+     *
+     * @param int|null $id
+     * @return array<string, int>
+     */
+    public function show(Request $request, $id = null)
+    {
+        // dump($id);
+        // dd($request->all());
+
+        $userClass = new User();
+
+        $data = $userClass->getUser($request->id);
+
+        return view('profile', $data);
+    }
+}
