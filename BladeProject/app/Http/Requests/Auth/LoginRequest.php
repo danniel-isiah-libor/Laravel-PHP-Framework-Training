9b2144fc7d08a8a3401b2dcc3d2b6466f8d2013b
JustNigel <nigelcamba@gmail.com>
2024-10-06 19:03:17 +0800

    D3

--- BladeProject/app/Http/Requests/Auth/LoginRequest.php
+++ BladeProject/app/Http/Requests/Auth/LoginRequest.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Models\User;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
@@ -41,14 +43,18 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
-            RateLimiter::hit($this->throttleKey());
+       $user = User::withTrashed()->whereEmail($this->email)->first();
 
-            throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
-            ]);
-        }
+       if($user &&
+       Hash::check($this->password,optional($user)->password)){
+        $user -> restore();
+       }
 
+       if(!Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))){
+        RateLimiter::hit($this->throttleKey());
+
+        throw ValidationException::withMessages(['email' => trans('auth.failed'),]);
+       }
         RateLimiter::clear($this->throttleKey());
     }
 
